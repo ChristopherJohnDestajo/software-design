6b989e75a4d23ced60b9f12af2ef069ea2275bc4
ChristopherJohnDestajo <c.destajo.546094@umindanao.edu.ph>
2025-06-03 11:54:15 +0800

    features/week6

--- lab6/Form1.Designer.cs
+++ lab6/Form1.Designer.cs
@@ -20,20 +20,145 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ListBoxBooks = new ListBox();
+            btnAddBook = new Button();
+            txtAuthorName = new TextBox();
+            txtBookTitle = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            btnUpdateBook = new Button();
+            btnDeletebook = new Button();
+            btnDisplaybook = new Button();
+            progressBar1 = new ProgressBar();
+            SuspendLayout();
+            // 
+            // ListBoxBooks
+            // 
+            ListBoxBooks.FormattingEnabled = true;
+            ListBoxBooks.Location = new Point(317, 39);
+            ListBoxBooks.Name = "ListBoxBooks";
+            ListBoxBooks.Size = new Size(417, 284);
+            ListBoxBooks.TabIndex = 0;
+            // 
+            // btnAddBook
+            // 
+            btnAddBook.Location = new Point(53, 39);
+            btnAddBook.Name = "btnAddBook";
+            btnAddBook.Size = new Size(210, 44);
+            btnAddBook.TabIndex = 1;
+            btnAddBook.Text = "add book";
+            btnAddBook.UseVisualStyleBackColor = true;
+            btnAddBook.Click += btnAddBook_Click;
+            // 
+            // txtAuthorName
+            // 
+            txtAuthorName.Location = new Point(44, 320);
+            txtAuthorName.Name = "txtAuthorName";
+            txtAuthorName.Size = new Size(219, 27);
+            txtAuthorName.TabIndex = 2;
+            // 
+            // txtBookTitle
+            // 
+            txtBookTitle.Location = new Point(44, 373);
+            txtBookTitle.Name = "txtBookTitle";
+            txtBookTitle.Size = new Size(219, 27);
+            txtBookTitle.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(44, 297);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Author";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(44, 350);
+            label2.Name = "label2";
+            label2.Size = new Size(76, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Book Title";
+            // 
+            // btnUpdateBook
+            // 
+            btnUpdateBook.Location = new Point(53, 89);
+            btnUpdateBook.Name = "btnUpdateBook";
+            btnUpdateBook.Size = new Size(210, 44);
+            btnUpdateBook.TabIndex = 6;
+            btnUpdateBook.Text = "update Book";
+            btnUpdateBook.UseVisualStyleBackColor = true;
+            btnUpdateBook.Click += btnUpdateBook_Click;
+            // 
+            // btnDeletebook
+            // 
+            btnDeletebook.Location = new Point(53, 139);
+            btnDeletebook.Name = "btnDeletebook";
+            btnDeletebook.Size = new Size(210, 44);
+            btnDeletebook.TabIndex = 7;
+            btnDeletebook.Text = "delete Book";
+            btnDeletebook.UseVisualStyleBackColor = true;
+            btnDeletebook.Click += btnDeletebook_Click;
+            // 
+            // btnDisplaybook
+            // 
+            btnDisplaybook.Location = new Point(53, 189);
+            btnDisplaybook.Name = "btnDisplaybook";
+            btnDisplaybook.Size = new Size(210, 44);
+            btnDisplaybook.TabIndex = 8;
+            btnDisplaybook.Text = "display Book";
+            btnDisplaybook.UseVisualStyleBackColor = true;
+            btnDisplaybook.Click += btnDisplaybook_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(327, 341);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(408, 33);
+            progressBar1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(progressBar1);
+            Controls.Add(btnDisplaybook);
+            Controls.Add(btnDeletebook);
+            Controls.Add(btnUpdateBook);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtBookTitle);
+            Controls.Add(txtAuthorName);
+            Controls.Add(btnAddBook);
+            Controls.Add(ListBoxBooks);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private ListBox ListBoxBooks;
+        private Button btnAddBook;
+        private TextBox txtAuthorName;
+        private TextBox txtBookTitle;
+        private Label label1;
+        private Label label2;
+        private Button btnUpdateBook;
+        private Button btnDeletebook;
+        private Button btnDisplaybook;
+        private ProgressBar progressBar1;
+    }
 }
--- lab6/Form1.cs
+++ lab6/Form1.cs
@@ -1,10 +1,159 @@
+
+using Form1;
+using Microsoft.EntityFrameworkCore;
 namespace lab6
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Author
+        {
+            public int AuthorId { get; set; }
+            public string Name { get; set; }
+            public virtual ICollection<Book> Books { get; set; }
+        }
+
+        public class Book
+        {
+            public int BookID { get; set; }
+            public string Title { get; set; }
+            public int AuthorId { get; set; }
+            public virtual Author Author { get; set; }
+        }
+
+
+        public async Task<List<string>> GetBooksAsync()
+        {
+
+            using (var context = new BookstoreContext())
+            {
+                var books = await context.Books.Include(b => b.Author)
+                        .Select(b => $"Book: {b.Title}, Author: {b.Author.Name}")
+                        .ToListAsync();
+
+                return books;
+            }
+        }
+        private async void btnFetchBooks_Click(object sender, EventArgs e)
+        {
+            var books = await GetBooksAsync();
+
+            ListBoxBooks.DataSource = books;
+        }
+
+        public async Task SaveBookAsync(string bookTitle, string authorName)
+        {
+            using (var context = new BookstoreContext())
+            {
+                var author = new Author { Name = authorName };
+                var book = new Book { Title = bookTitle, Author = author };
+
+
+                context.Authors.Add(author);
+                context.Books.Add(book);
+
+                await context.SaveChangesAsync();
+            }
+        }
+
+        private async void btnAddBook_Click(object sender, EventArgs e)
+        {
+            var authorName = txtAuthorName.Text;
+            var bookTitle = txtBookTitle.Text;
+
+            await SaveBookAsync(bookTitle, authorName);
+
+            MessageBox.Show("Book and Author saved Successfully");
+        }
+
+        //Task
+        public async Task UpdateBookAsync(string currentTitle, string newTitle, string newAuthorName)
+        {
+            using (var context = new BookstoreContext())
+            {
+                var book = await context.Books.Include(b => b.Author)
+                                 .FirstOrDefaultAsync(b => b.Title == currentTitle);
+                if (book != null)
+                {
+                    book.Title = newTitle;
+                    book.Author.Name = newAuthorName;
+                    await context.SaveChangesAsync();
+                }
+                else
+                {
+                    MessageBox.Show("Book not found.");
+                }
+            }
+        }
+
+        private  async void btnUpdateBook_Click(object sender, EventArgs e)
+        {
+
+            string currentTitle = txtBookTitle.Text;
+            string newTitle = txtBookTitle.Text;
+            string newAuthor = txtAuthorName.Text;
+
+            await UpdateBookAsync(currentTitle, newTitle, newAuthor);
+            MessageBox.Show("Book updated successfully!");
+        }
+
+        //Delets book
+
+        public async Task DeleteBookByTitleAsync(string title)
+        {
+            using (var context = new BookstoreContext())
+            {
+                var book = await context.Books.FirstOrDefaultAsync(b => b.Title == title);
+                if (book != null)
+                {
+                    context.Books.Remove(book);
+                    await context.SaveChangesAsync();
+                }
+                else
+                {
+                    MessageBox.Show("Book not found.");
+                }
+            }
+        }
+
+        private  async void btnDeletebook_Click(object sender, EventArgs e)
+        {
+            string title = txtBookTitle.Text;
+            await DeleteBookByTitleAsync(title);
+            MessageBox.Show("Book deleted successfully!");
+        }
+
+        //Display With Progress bar
+
+        public async Task<List<string>> GetBooksAsyncWithProgress(IProgress<int> progress)
+        {
+            using (var context = new BookstoreContext())
+            {
+                var books = await context.Books.Include(b => b.Author).ToListAsync();
+                var result = new List<string>();
+                int count = books.Count;
+
+                for (int i = 0; i < count; i++)
+                {
+                    result.Add($"Book: {books[i].Title}, Author: {books[i].Author.Name}");
+                    progress.Report((i + 1) * 100 / count);
+                    await Task.Delay(100); // Simulate load
+                }
+
+                return result;
+            }
+        }
+
+        private  async void btnDisplaybook_Click(object sender, EventArgs e)
+        {
+            var progress = new Progress<int>(value => progressBar1.Value = value);
+            progressBar1.Value = 0;
+
+            var books = await GetBooksAsyncWithProgress(progress);
+            ListBoxBooks.DataSource = books;
+        }
+    }
 }
