497c74df69742f494f50ee61c4620ffed8c5d75b
ChristopherJohnDestajo <c.destajo.546094@umindanao.edu.ph>
2025-06-03 09:43:23 +0800

    features/Week5

---
+++ Form1/BookstoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Form1
+{
+    public class BookstoreContext :DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+    }
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+    //    optionsBuilder.UseApplicationServiceProvider();
+    //}
+}
--- Form1/Form1.cs
+++ Form1/Form1.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
 namespace Form1
 {
     public partial class Form1 : Form
@@ -6,5 +11,84 @@ namespace Form1
         {
             InitializeComponent();
         }
+        public class Author
+        {
+            public int AuthorId { get; set; }
+            public string Name { get; set; }
+            public virtual ICollection<Book> Books { get; set; }
+        }
+
+        public class Book
+        {
+            public int BookID { get; set; }
+            public string title { get; set; }
+            public int AuthorId { get; set; }
+            public virtual Author Author { get; set; }
+        }
+
+        public void AddAuthorwithBook(string authorName, string BookTitle)
+        {
+            using (var context = new BookstoreContext())
+            {
+                var author = new Author { Name = authorName };
+                var book = new Book { title = BookTitle, Author = author };
+
+                context.Add(author);
+                context.Add(book);
+                context.SaveChanges();
+
+            }
+        }
+
+        public List<string> DisplayBooks()
+        {
+            using (var context = new BookstoreContext())
+            {
+                return context.Books.Include(b => b.Author)
+                    .Select(b => $"Book: {b.title}, Author: {b.Author.Name}")
+                    .ToList();
+            }
+        }
+
+
+        public void UpdateBookTitle(int bookId, string newTitle)
+        {
+            using (var context = new BookstoreContext())
+            {
+                var book = context.Books.Find(bookId);
+                if (book != null)
+                {
+                    book.title = newTitle;
+                    context.SaveChanges();
+                }
+                else
+                {
+                    MessageBox.Show("Book not found.");
+                }
+            }
+        }
+
+        //  Delete book by BookID
+        public void DeleteBook(int bookId)
+        {
+            using (var context = new BookstoreContext())
+            {
+                var book = context.Books.Find(bookId);
+                if (book != null)
+                {
+                    context.Books.Remove(book);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    MessageBox.Show("Book not found.");
+                }
+
+
+
+
+            }
+        }
     }
 }
+
