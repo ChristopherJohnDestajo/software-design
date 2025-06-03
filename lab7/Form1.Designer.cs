3c972dadd3fa8fd294b74f080dd8a167a4fa31ed
ChristopherJohnDestajo <c.destajo.546094@umindanao.edu.ph>
2025-06-03 13:03:58 +0800

    features/Weel7

--- lab7/Form1.Designer.cs
+++ lab7/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace lab7
 {
-	partial class Form1
+	partial class txtAuthorName
 	{
 		/// <summary>
 		///  Required designer variable.
@@ -20,20 +20,182 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnNextPage = new Button();
+            btnreverse = new Button();
+            listBoxBooks = new ListBox();
+            lblPageNumber = new Label();
+            addbook = new Button();
+            txtNameAuthor = new TextBox();
+            txtBookTitle = new TextBox();
+            btnExporter = new Button();
+            AsyncSearch = new Button();
+            txtSearch = new TextBox();
+            Importer = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // btnNextPage
+            // 
+            btnNextPage.Location = new Point(547, 314);
+            btnNextPage.Name = "btnNextPage";
+            btnNextPage.Size = new Size(198, 69);
+            btnNextPage.TabIndex = 0;
+            btnNextPage.Text = "Next";
+            btnNextPage.UseVisualStyleBackColor = true;
+            btnNextPage.Click += btnNextPage_Click;
+            // 
+            // btnreverse
+            // 
+            btnreverse.Location = new Point(316, 308);
+            btnreverse.Name = "btnreverse";
+            btnreverse.Size = new Size(198, 69);
+            btnreverse.TabIndex = 1;
+            btnreverse.Text = "Previous ";
+            btnreverse.UseVisualStyleBackColor = true;
+            btnreverse.Click += btnreverse_Click;
+            // 
+            // listBoxBooks
+            // 
+            listBoxBooks.FormattingEnabled = true;
+            listBoxBooks.Location = new Point(316, 38);
+            listBoxBooks.Name = "listBoxBooks";
+            listBoxBooks.Size = new Size(429, 244);
+            listBoxBooks.TabIndex = 2;
+            // 
+            // lblPageNumber
+            // 
+            lblPageNumber.AutoSize = true;
+            lblPageNumber.Location = new Point(357, 285);
+            lblPageNumber.Name = "lblPageNumber";
+            lblPageNumber.Size = new Size(0, 20);
+            lblPageNumber.TabIndex = 3;
+            // 
+            // addbook
+            // 
+            addbook.Location = new Point(24, 116);
+            addbook.Name = "addbook";
+            addbook.Size = new Size(154, 39);
+            addbook.TabIndex = 4;
+            addbook.Text = "Add Book";
+            addbook.UseVisualStyleBackColor = true;
+            addbook.Click += addbook_Click;
+            // 
+            // txtNameAuthor
+            // 
+            txtNameAuthor.Location = new Point(24, 32);
+            txtNameAuthor.Name = "txtNameAuthor";
+            txtNameAuthor.Size = new Size(186, 27);
+            txtNameAuthor.TabIndex = 5;
+            // 
+            // txtBookTitle
+            // 
+            txtBookTitle.Location = new Point(24, 83);
+            txtBookTitle.Name = "txtBookTitle";
+            txtBookTitle.Size = new Size(186, 27);
+            txtBookTitle.TabIndex = 6;
+            // 
+            // btnExporter
+            // 
+            btnExporter.Location = new Point(24, 161);
+            btnExporter.Name = "btnExporter";
+            btnExporter.Size = new Size(154, 45);
+            btnExporter.TabIndex = 7;
+            btnExporter.Text = "Export Book";
+            btnExporter.UseVisualStyleBackColor = true;
+            btnExporter.Click += btnExporter_Click;
+            // 
+            // AsyncSearch
+            // 
+            AsyncSearch.Location = new Point(70, 341);
+            AsyncSearch.Name = "AsyncSearch";
+            AsyncSearch.Size = new Size(140, 45);
+            AsyncSearch.TabIndex = 8;
+            AsyncSearch.Text = "Search Book";
+            AsyncSearch.UseVisualStyleBackColor = true;
+            AsyncSearch.Click += AsyncSearch_Click;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Location = new Point(12, 308);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(278, 27);
+            txtSearch.TabIndex = 9;
+            // 
+            // Importer
+            // 
+            Importer.Location = new Point(24, 212);
+            Importer.Name = "Importer";
+            Importer.Size = new Size(154, 45);
+            Importer.TabIndex = 10;
+            Importer.Text = "Import Book";
+            Importer.UseVisualStyleBackColor = true;
+            Importer.Click += Importer_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(24, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(92, 20);
+            label1.TabIndex = 11;
+            label1.Text = "Book Author";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(24, 60);
+            label2.Name = "label2";
+            label2.Size = new Size(76, 20);
+            label2.TabIndex = 12;
+            label2.Text = "Book Title";
+            // 
+            // txtAuthorName
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(Importer);
+            Controls.Add(txtSearch);
+            Controls.Add(AsyncSearch);
+            Controls.Add(btnExporter);
+            Controls.Add(txtBookTitle);
+            Controls.Add(txtNameAuthor);
+            Controls.Add(addbook);
+            Controls.Add(lblPageNumber);
+            Controls.Add(listBoxBooks);
+            Controls.Add(btnreverse);
+            Controls.Add(btnNextPage);
+            Name = "txtAuthorName";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private Button btnNextPage;
+        private Button btnreverse;
+        private ListBox listBoxBooks;
+        private Label lblPageNumber;
+        private Button addbook;
+        private TextBox txtNameAuthor;
+        private TextBox txtBookTitle;
+        private Button btnExporter;
+        private Button AsyncSearch;
+        private TextBox txtSearch;
+        private Button Importer;
+        private Label label1;
+        private Label label2;
+    }
 }
--- lab7/Form1.cs
+++ lab7/Form1.cs
@@ -1,10 +1,201 @@
+
+using Microsoft.EntityFrameworkCore;
+using System.Web.Mvc;
 namespace lab7
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class txtAuthorName : Form
+    {
+        public txtAuthorName()
+        {
+            InitializeComponent();
+        }
+
+        private const int pageSize = 10;
+        private int currentPage = 1;
+
+        public string Filter { get; private set; }
+        public string Title { get; private set; }
+
+        public class Author
+        {
+            public int AuthorId { get; set; }
+            public string Name { get; set; }
+            public virtual ICollection<Book> Books { get; set; }
+        }
+
+        public class Book
+        {
+            public int BookID { get; set; }
+            public string Title { get; set; }
+            public int AuthorId { get; set; }
+            public virtual Author Author { get; set; }
+        }
+
+        public async Task<List<string>> GetBooksByPageAsync(int pageNumber)
+        {
+            using (var context = new BookstoreContext())
+            {
+                var books = await context.Books
+                                            .Include(x => x.Author)
+                                            .OrderBy(x => x.BookID)
+                                            .Skip((pageNumber - 1) * pageSize)
+                                            .Take(pageSize)
+                                            .Select(x => x.Title)
+                                            .ToListAsync();
+
+                return books;
+            }
+        }
+
+        private async void btnNextPage_Click(object sender, EventArgs e)
+        {
+            currentPage++;
+            var books = await GetBooksByPageAsync(currentPage);
+            listBoxBooks.DataSource = books;
+            lblPageNumber.Text = $"Page{currentPage}";
+        }
+
+        private async void btnreverse_Click(object sender, EventArgs e)
+        {
+            currentPage--;
+            var books = await GetBooksByPageAsync(currentPage);
+            listBoxBooks.DataSource = books;
+            lblPageNumber.Text = $"Page{currentPage}";
+        }
+
+        public async Task SaveBookWithHandlingAsync(string bookTitle, string authorName)
+        {
+            try
+            {
+                using (var context = new BookstoreContext())
+                {
+                    var author = new Author { Name = authorName };
+                    var book = new Book { Title = bookTitle, Author = author };
+
+                    context.Authors.Add(author);
+                    context.Books.Add(book);
+
+                    await context.SaveChangesAsync();
+                    MessageBox.Show("Books and Author saved Successfully");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occured :{ex.Message}");
+            }
+        }
+
+        private async void addbook_Click(object sender, EventArgs e)
+        {
+            var authorName = txtNameAuthor.Text;
+            var bookTitle = txtBookTitle.Text;
+
+            await SaveBookWithHandlingAsync(bookTitle, authorName);
+        }
+        public async Task ExportBooksAsync(string filePath)
+        {
+            var books = await GetBooksByPageAsync(currentPage);
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (var book in books)
+                {
+                    await writer.WriteLineAsync(book);
+                }
+            }
+        }
+
+        private async void btnExporter_Click(object sender, EventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog();
+            {
+                Filter = "Text Files (*.text)|*.txt";
+                Title = "Save Book List";
+            };
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                await ExportBooksAsync(filePath);
+                MessageBox.Show("books exported Successfully");
+            }
+        }
+
+
+
+        //Asychronous Search w/ error handling
+        public async Task<List<string>> SearchBooksAsync(string keyword)
+        {
+            try
+            {
+                using (var context = new BookstoreContext())
+                {
+                    var results = await context.Books
+                        .Include(b => b.Author)
+                        .Where(b => b.Title.Contains(keyword))
+                        .Select(b => $"Title: {b.Title}, Author: {b.Author.Name}")
+                        .ToListAsync();
+
+                    return results;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Search failed: {ex.Message}");
+                return new List<string>();
+            }
+        }
+
+        private async void AsyncSearch_Click(object sender, EventArgs e)
+        {
+            string keyword = txtSearch.Text;
+            var results = await SearchBooksAsync(keyword);
+            listBoxBooks.DataSource = results;
+        }
+
+        //Asychronous Data Import
+        public async Task ImportBooksAsync(string filePath)
+        {
+            try
+            {
+                var lines = await File.ReadAllLinesAsync(filePath);
+
+                using (var context = new BookstoreContext())
+                {
+                    foreach (var line in lines)
+                    {
+                        var parts = line.Split('|');
+                        if (parts.Length == 2)
+                        {
+                            var author = new Author { Name = parts[0].Trim() };
+                            var book = new Book { Title = parts[1].Trim(), Author = author };
+
+                            context.Authors.Add(author);
+                            context.Books.Add(book);
+                        }
+                    }
+                    await context.SaveChangesAsync();
+                    MessageBox.Show("Books imported successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Import failed: {ex.Message}");
+            }
+        }
+
+        private  async void Importer_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text Files (*.txt)|*.txt",
+                Title = "Import Book List"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                await ImportBooksAsync(filePath);
+            }
+        }
+    }
 }
